a382d91ac713b5170e34e2e087734c1f1849bb28
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2020-06-21 21:12:26 +0100

    tweaks

--- stock-screening-tool.py
+++ stock-screening-tool.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import quandl
 
-url = 'http://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-df = pd.read_html(url)
+quandl.ApiConfig.api_key = 'hm8hEsEo4aE6iE_yTGjw'
 
-tickers = [ticker for ticker in df[0]['Symbol']]
-
-
-print(len(tickers))
+file = 'https://s3.amazonaws.com/quandl-production-static/BSE+Descriptions/stocks.txt'
+df = pd.read_csv(file, sep='|', header=0)
+#df = df['CODE']
+print(df.STOCK[1500][:-11])
+#print(quandl.get('BSE/'+df['CODE'][400])['Close'][-1])
