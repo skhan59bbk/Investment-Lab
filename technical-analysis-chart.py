1aec57c9566eb36d5004dff0102015bd917eda93
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2020-07-16 15:46:04 +0100

    TA chart initial

---
+++ technical-analysis-chart.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+file = 'C://Users//samee//Documents//Datasets//EOD-AAPL.csv'
+
+df = pd.read_csv(file)
+df = df[['Date', 'Adj_Open', 'Adj_High', 'Adj_Low', 'Adj_Close', 'Adj_Volume']]
+df = df.rename(columns={'Adj_Open':'Open', 'Adj_High':'High', 'Adj_Low':'Low', 'Adj_Close':'Close', 'Adj_Volume':'Volume'})
+#df = df.set_index('Date')
+
+#print(df.head())
+
+
+def sma(series, periods):
+
+    df['sma_'+str(periods)] = df[series].rolling(window=periods).mean()
+
+    return df['sma_'+str(periods)]
+
+def ema(series, periods):
+    multiplier = 2 / (periods + 1)
+
+    conditions = [(df.index < periods), (df.index >= periods)]
+    values = [sma('Close', periods), 1]
+
+    df['ema_'+str(periods)] = np.select(conditions, values)
+
+    for i in range(periods, len(df)):
+        df['ema_'+str(periods)].loc[i] = (df['Close'].loc[i] * multiplier) + (df['ema_'+str(periods)].loc[i-1] * (1 - multiplier))
+    
+    #print(df['ema_'+str(periods)].head(30))
+    return df['ema_'+str(periods)]
+    
+    
+def rsi(periods):
+    pass
+
+
+def macd(fast, slow, signal):
+    df['macd'] = sma('Close', fast) - sma('Close', slow)
+    df['macd_signal'] = df['macd'].rolling(window=signal).mean()
+    #print(df.head(20))
+    return df[['macd', 'macd_signal']]
+
+
+##fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+##df['Close'].plot(ax=ax[0,0])
+###sma(50).plot()
+##macd(12, 26, 9).plot(ax=ax[0,1])
+##plt.show()
+
+#print(df.tail(20))
+
+df.Close.plot()
+ema('Close', 15).plot()
+plt.show()
